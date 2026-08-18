9f5431bbb82f13df9a67ee0f392a166ee01d17bd
ikica007 <108173486+ikica007@users.noreply.github.com>
2026-08-18 13:19:22 +0200

    Update Buketi.tsx

--- src/components/categories/Buketi.tsx
+++ src/components/categories/Buketi.tsx
@@ -22,7 +22,25 @@ const CLOUDINARY_IMAGES = [
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1783354824/buketi3_juremx.jpg",
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1783354822/buketi51_kfptye.jpg",
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1783354823/buketi2_pmi14s.jpg",
-  "https://res.cloudinary.com/de7gefkxl/image/upload/v1783354823/buketi1_qr1ta5.jpg"
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1783354823/buketi1_qr1ta5.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049720/buketi68_itvsvc.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049722/buketi51_rcdutt.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049723/buketi50_zibwfb.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049725/buketi54_nmfonc.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049726/buketi55_sxbf4b.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049726/buketi56_ky9w6t.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049726/buketi52_hwzmzf.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049727/buketi53_plof4r.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049729/buketi60_xpddbl.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049729/buketi57_vrlflv.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049729/buketi59_yd8spp.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049730/buketi58_yqnmro.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049731/buketi61_idpe90.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049732/buketi63_nsh1oq.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049732/buketi62_kiqvba.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049733/buketi65_h2wbam.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049736/buketi67_fawzta.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787049734/buketi66_m3vuxa.jpg"
 ];
 
 export function Buketi({ onBack }: Props) {
