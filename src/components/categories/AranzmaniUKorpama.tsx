717b6f545b2449fe4026708cd7fb948b40641fbc
ikica007 <108173486+ikica007@users.noreply.github.com>
2026-08-19 13:29:29 +0200

    Update AranzmaniUKorpama.tsx

--- src/components/categories/AranzmaniUKorpama.tsx
+++ src/components/categories/AranzmaniUKorpama.tsx
@@ -5,6 +5,7 @@ interface Props {
 }
 
 const CLOUDINARY_IMAGES = [
+  // Originalnih 25 slika za koje postoje tacne cijene u prices.ts
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991026/aranzmaniukorpama35_oncgd7.jpg",
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991025/aranzmaniukorpama36_qbwnny.jpg",
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991025/aranzmaniukorpama38_qsj5j6.jpg",
@@ -29,7 +30,23 @@ const CLOUDINARY_IMAGES = [
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991009/aranzmaniukorpama1_fvf2av.jpg",
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991010/aranzmaniukorpama3_crhdcf.jpg",
   "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991010/aranzmaniukorpama2_fqq7pk.jpg",
-  "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991008/aranzmaniukorpama39_w6gz0c.jpg"
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1782991008/aranzmaniukorpama39_w6gz0c.jpg",
+  
+  // Novi dodaci (slike koje su nedostajale) - one ce dobiti oznaku "Na upit"
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137964/aranzmaniukorpama6_a8g9zd.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137965/aranzmaniukorpama9_w7zjpy.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137966/aranzmaniukorpama11_x12fhb.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137966/aranzmaniukorpama12_rcc3tl.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137967/aranzmaniukorpama13_kvklue.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137968/aranzmaniukorpama18_yvfmps.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137969/aranzmaniukorpama19_xz4ccp.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137969/aranzmaniukorpama22_uju5ro.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137969/aranzmaniukorpama23_merd5k.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137970/aranzmaniukorpama24_szlfgu.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137969/aranzmaniukorpama25_fnzbhv.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137971/aranzmaniukorpama28_gv2x8t.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137971/aranzmaniukorpama30_gnr0fr.jpg",
+  "https://res.cloudinary.com/de7gefkxl/image/upload/v1787137972/aranzmaniukorpama33_swyubk.jpg"
 ];
 
 export function AranzmaniUKorpama({ onBack }: Props) {
